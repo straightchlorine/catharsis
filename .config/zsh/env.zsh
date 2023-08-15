f886b1842e0fa3e47268298f3434d0a4eedd62db
Piotr Krzysztof Lis <piotrlis555@gmail.com>
2023-08-15 18:09:43 +0200

    zsh: aliases and env variables

---
+++ .config/zsh/env.zsh
@@ -0,0 +1,29 @@
+path+=($HOME/automation)
+path+=($HOME/.venv/lib)
+export PATH
+
+export TERMINAL="kitty"
+export TERM="xterm-256color"
+
+export STARSHIP_CONFIG=$HOME/.config/starship/starship.toml
+export STARSHIP_CACHE=$HOME/.config/starship/cache
+
+export SUDO_PROMPT="password: "
+
+export BROWSER="firefox"
+export VISUAL="nvim"
+export EDITOR="nvim"
+
+# commit sign prompt
+export GPG_TTY=$(tty)
+
+# aliases
+alias vim="nvim"
+
+alias ls='exa -al --color=always --group-directories-first'
+alias ll='exa -l --color=always --group-directories-first'
+alias sl='exa -l --color=always --group-directories-first'
+
+alias gitlog="git log --graph --abbrev-commit --decorate --format=format:'%C(bold blue)%h%C(reset) - %C(bold green)(%ar)%C(reset) %C(white)%s%C(reset) %C(dim white)- %an%C(reset)%C(auto)%d%C(reset)' --all"
+
+
