edf6a14fc3c6fa2d55964482edefc51440eb69de
Piotr Krzysztof Lis <piotrlis555@gmail.com>
2023-10-28 19:45:13 +0200

    zsh: added conda configuration snippet along with completion for please

--- .config/zsh/.zshrc
+++ .config/zsh/.zshrc
@@ -7,14 +7,30 @@ source $ZDOTDIR/env.zsh
 # The following lines were added by compinstall
 zstyle ':completion:*' completer _expand _complete _ignored _correct _approximate
 zstyle ':completion:*' max-errors 2 numeric
-zstyle :compinstall filename '/home/goldberg/.zshrc'
+zstyle :compinstall filename '$HOME/.zshrc'
+
+# conda setup
+__conda_setup="$('$HOME/.local/share/anaconda/bin/conda' 'shell.bash' 'hook' 2> /dev/null)"
+if [ $? -eq 0 ]; then
+    eval "$__conda_setup"
+else
+    if [ -f "$HOME/.local/share/anaconda/etc/profile.d/conda.sh" ]; then
+        . "$HOME/.local/share/anaconda/etc/profile.d/conda.sh"
+    else
+        export PATH="$HOME/.local/share/anaconda/bin:$PATH"
+    fi
+fi
+unset __conda_setup 
 
 # End of lines added by compinstall
 # Lines configured by zsh-newuser-install
-HISTFILE=~/.config/zsh/.histfile
+HISTFILE=$HOME/.config/zsh/.histfile
 HISTSIZE=1000
 SAVEHIST=1000
 # End of lines configured by zsh-newuser-install
 
 # vim mode
 bindkey -v
+
+# please completion
+source <(plz --completion_script)
---
+++ .config/zsh/plugins.zsh
@@ -0,0 +1,54 @@
+# Configure and load plugins using Zinit's
+ZINIT_HOME="${ZINIT_HOME:-${XDG_DATA_HOME:-${HOME}/.local/share}/zinit}"
+
+### Added by Zinit's installer
+if [[ ! -f $HOME/.local/share/zinit/zinit.git/zinit.zsh ]]; then
+    print -P "%F{33} %F{220}Installing %F{33}ZDHARMA-CONTINUUM%F{220} Initiative Plugin Manager (%F{33}zdharma-continuum/zinit%F{220})…%f"
+    command mkdir -p "$HOME/.local/share/zinit" && command chmod g-rwX "$HOME/.local/share/zinit"
+    command git clone https://github.com/zdharma-continuum/zinit "$HOME/.local/share/zinit/zinit.git" && \
+        print -P "%F{33} %F{34}Installation successful.%f%b" || \
+        print -P "%F{160} The clone has failed.%f%b"
+fi
+
+source "$HOME/.local/share/zinit/zinit.git/zinit.zsh"
+
+zinit ice blockf atpull'zinit creinstall -q .'
+zinit light zsh-users/zsh-completions
+
+autoload compinit
+compinit
+
+zinit light-mode for \
+  zdharma-continuum/zinit-annex-as-monitor \
+  zdharma-continuum/zinit-annex-bin-gem-node \
+  zdharma-continuum/zinit-annex-patch-dl \
+  zdharma-continuum/zinit-annex-rust \
+  zdharma-continuum/fast-syntax-highlighting \
+  hlissner/zsh-autopair \
+  MichaelAquilina/zsh-you-should-use \
+  zsh-users/zsh-autosuggestions \
+  Aloxaf/fzf-tab
+
+zinit ice wait'3' lucid
+zinit light zsh-users/zsh-history-substring-search
+
+zinit ice wait'2' lucid
+zinit light zdharma-continuum/history-search-multi-word
+
+zinit ice from"gh-r" as"command"
+zinit light junegunn/fzf-bin
+
+zinit ice wait lucid from"gh-r" as"program" mv"bin/exa* -> exa"
+zinit light ogham/exa
+
+zinit ice wait lucid
+zinit light changyuheng/zsh-interactive-cd
+
+zinit ice depth=1
+zinit light jeffreytse/zsh-vi-mode
+
+zinit ice as"command" from"gh-r" \
+          atclone"./starship init zsh > init.zsh; ./starship completions zsh > _starship" \
+          atpull"%atclone" src"init.zsh"
+
+zinit light starship/starship
